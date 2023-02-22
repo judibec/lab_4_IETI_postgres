4f997c5e6c24a5b2c30179ee83a6d014da2c42cf
judibec <juanchopolo6954@gmail.com>
2023-02-22 11:53:19 -0500

    connection with postgres

---
+++ src/main/java/com/example/postgresql/controller/UserController.java
@@ -0,0 +1,35 @@
+package com.example.postgresql.controller;
+
+import com.example.postgresql.entity.User;
+import com.example.postgresql.entity.UserDto;
+import com.example.postgresql.exception.UserNotFoundException;
+import com.example.postgresql.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/v1/user")
+public class UserController {
+    private final UserRepository userRepository;
+
+    public UserController(@Autowired UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @PostMapping
+    User createUser(@RequestBody UserDto userDto) {
+        User user = new User(userDto);
+        return userRepository.save(user);
+    }
+
+    @GetMapping("/{id}")
+    User findById(@PathVariable Long id) {
+        Optional<User> optionalUser = userRepository.findById(id);
+        if (optionalUser.isPresent())
+            return optionalUser.get();
+        else throw new UserNotFoundException();
+    }
+
+}
